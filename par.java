187b25793d92243070d47a6c4265d3c826a9670c
jacobcmalimban <jacobcmalimban@gmail.com>
2019-10-08 21:56:48 +0000

    Expected Outputs Achieved!
    Program now slices sequences into m-long sizes and properly creates d=1 hamming different permutations.
    
    This is the working version, so use it.
    
    Resolves: #3
    
    NTS: Hashsets are interesting. It took me a while to realize I needed addAll() instead of retainAll(), but I finally got it.

--- par.java
+++ par.java
@@ -15,9 +15,10 @@ import java.lang.*;
 import java.io.*;
 import java.util.*;
 
-public class seq {
+public class par {
 	private static Semaphore sem = new Semaphore(1);
 	private static HashSet<String> hams = new HashSet<String>();
+	private static HashSet<String> hamsCopy = new HashSet<String>();
 	private static int d, m, n, l;
 
 	public static void main(String[] args) {
@@ -54,23 +55,43 @@ public class seq {
 		l = seqIn.get(0).length();
 		int slicount = l-m+1;
 		int d = 1;
+		HashSet<String> tempHams = new HashSet<String>();
 
-		for(int i = 0; i < slicount; i++) { // each m-long dna slice
-			hams.add( seqIn.get(0).substring(i, i+m) );
+
+		for(int i = 0; i < n; i++) {
+			hams = new HashSet<String>();
+			hamsCopy = new HashSet<String>();
+
+			for(int j = 0; j < slicount; j++) { // each m-long dna slice
+				hams.add( seqIn.get(i).substring(j, j+m) ); // only pure slices
+			}
+System.out.println("\n\n"+hams+"\n");
+			// create threads
+			Thread[] threads = new Thread[4];
+			for(int j = 0; j < threads.length; j++) {
+				threads[j] = new Thread(new Volunteer("Volunteer " + j, j));
+				threads[j].start();
+			}
+
+			// wait for all sequence1's hams to exist
+			for(int j = 0; j < threads.length; j++)
+				try {
+					threads[j].join();
+				} catch (Exception e) { System.out.println("lol threads gone cray ;((("); }
+
+			hams.addAll(hamsCopy);
+			// hams = valid seqIn(i) hams
+			if ( i == 0 ) {
+				tempHams = new HashSet<String>(hams); // save verified previous hams
+			} else {
+				tempHams.retainAll(hams);
+			}
+System.out.println("temphams: " + tempHams);
+
 		}
 
-		// create threads
-		Thread[] threads = new Thread[4];
-		for(int i = 0; i < threads.length; i++) {
-			threads[i] = new Thread(new Volunteer("Volunteer " + i, i);
-			threads[i].start();
-		}
-
-		// wait for all sequence1's hams to exist
-		for(int i = 0; i < threads.length; i++)
-			try {
-				threads[i].join();
-			} catch (Exception e) { System.out.println("lol threads gone cray ;((("); }
+		System.out.println("Valid hams: " + tempHams);
+	}
 
 /** /
 
@@ -118,40 +139,80 @@ YES THERE IS
 		// 1, 3
 		// 1, 2
 
-		for (int i = 1; i < hams.length; i++)
+/** /		for (int i = 1; i < hams.size(); i++)
 			hams[0].retainAll(hams[i]);
 		System.out.println(hams[0]);
+/**/
 
-		}
-	}
-}
+	private static class Volunteer implements Runnable {
+		private String name;
+		private int id;
+		private String proteins = "ACTG";
 
-private static class Volunteer implements Runnable {
-	private String name;
-	private int id;
-
-	pubic Volunteer(String name, int id) {
-		this.name = name;
-		this.id = id;
-	}
-
-	public void run() {
-		private static HashSet<String> ham = new HashSet<String>();
-		while( hams.iterator().next() ) { // iterate through each legal
-			// if GGAG
-			/*
-				AGAG
-				GAAG
-				GGAG
-				GGAA
-			*/
-			// add to own ham
-
+		public Volunteer(String name, int id) {
+			this.name = name;
+			this.id = id;
 		}
 
-		// combine with original
-		sem.acquire();
-		hams.addAll(ham);
-		sem.release();
+		public void run() {
+			HashSet<String> ham = new HashSet<String>();
+			for( String s : new HashSet<String>(hams) ) { // iterate through each legal
+//				System.out.println(id + ":" + s);// if GGAG
+				/*
+					AGAG
+					GAAG
+					GGAG
+					GGAA
+				*/
+				// add to own ham
+
+				for ( int i = 0; i < s.length(); i++) { // d = 1
+					if (id == 3)
+						System.out.print( s.substring(0,i)
+ .concat(proteins.substring(id,id+1)) .concat(s.substring(i+1)) + " " );
+
+					ham.add( s.substring(0,i) .concat(proteins.substring(id,id+1)) .concat(s.substring(i+1)) );
+				}
+
+			}
+/* */
+			// combine with original
+			sem.acquire();
+			System.out.println(id+":"+ham.size());
+			hamsCopy.addAll(ham);
+			sem.release();
+/* */
+		}
+	}
+
+	private static class Volunteer2 implements Runnable {
+		private String name;
+		private int id;
+
+		public Volunteer2(String name, int id) {
+			this.name = name;
+			this.id = id;
+		}
+
+		public void run() {
+			HashSet<String> ham = new HashSet<String>();
+			for( String s : hams ) { // iterate through each legal
+				System.out.println(id + ":" + s);// if GGAG
+				/*
+					AGAG
+					GAAG
+					GGAG
+					GGAA
+				*/
+				// add to own ham
+				ham.add(s);
+
+			}
+
+			// combine with original
+			sem.acquire();
+			hams.addAll(ham);
+			sem.release();
+		}
 	}
 }
